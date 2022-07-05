ea3df44dea78c51c81004329b367fa4081a9ae33
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-07-05 23:36:44 +0300

    .

---
+++ components/Layout.js
@@ -0,0 +1,69 @@
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Link,
+  createMuiTheme,
+  ThemeProvider,
+  CssBaseline,
+} from '@material-ui/core';
+import Head from 'next/head';
+import React from 'react';
+import useStyles from '../utils/styles';
+import NextLink from 'next/link';
+
+export default function Layout({ description, title, children }) {
+  const theme = createMuiTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+    },
+  });
+  const classes = useStyles();
+  return (
+    <div>
+      <Head>
+        <title>
+          {title ? `${title} - Yilmaz Ecoomerce` : 'Yilmaz Ecoomerce'}
+        </title>
+        {description && <meta name="description" content={description}></meta>}
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>
+                  Yilmaz Ecoomerce
+                </Typography>
+              </Link>
+            </NextLink>
+            <div className={classes.grow}></div>
+            <div>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All rigth is reserved. Yilmaz Tasdelen</Typography>
+        </footer>
+      </ThemeProvider>
+    </div>
+  );
+}
